89d212325d607bb5a04f96f4ca750e984c3d0313
andrewoculus <andrewoculus@gmail.com>
2021-11-03 18:15:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace TimeProg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.Grafik1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ScrollBarXmax = new System.Windows.Forms.VScrollBar();
+            this.ScrollBarXmin = new System.Windows.Forms.VScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(12, 559);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(207, 45);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Загрузить скрипт";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(1254, 559);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(209, 45);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Сохранить в файл";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Grafik1
+            // 
+            this.Grafik1.Location = new System.Drawing.Point(599, 559);
+            this.Grafik1.Name = "Grafik1";
+            this.Grafik1.Size = new System.Drawing.Size(260, 45);
+            this.Grafik1.TabIndex = 2;
+            this.Grafik1.Text = "Построить график";
+            this.Grafik1.UseVisualStyleBackColor = true;
+            this.Grafik1.Click += new System.EventHandler(this.Grafik1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(168, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1295, 541);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            title1.Name = "Title1";
+            title1.Text = "Циклограмма";
+            this.chart1.Titles.Add(title1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(34, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = " Xmin";
+            // 
+            // ScrollBarXmax
+            // 
+            this.ScrollBarXmax.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ScrollBarXmax.Location = new System.Drawing.Point(12, 35);
+            this.ScrollBarXmax.Name = "ScrollBarXmax";
+            this.ScrollBarXmax.Size = new System.Drawing.Size(19, 524);
+            this.ScrollBarXmax.TabIndex = 6;
+            // 
+            // ScrollBarXmin
+            // 
+            this.ScrollBarXmin.Location = new System.Drawing.Point(51, 35);
+            this.ScrollBarXmin.Name = "ScrollBarXmin";
+            this.ScrollBarXmin.Size = new System.Drawing.Size(19, 524);
+            this.ScrollBarXmin.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Xmax";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1475, 616);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ScrollBarXmin);
+            this.Controls.Add(this.ScrollBarXmax);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.Grafik1);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button Grafik1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.VScrollBar ScrollBarXmax;
+        private System.Windows.Forms.VScrollBar ScrollBarXmin;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TimeProg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+
+            chart1.MouseWheel += chart1_MouseWheel;
+        }
+        string PathRead = "0", InputFile = "0", PathWrite = "0";
+        double T = 0, S = 0, V = 0, A = 0, LastPosition = 0, FinalTime = 0;
+
+        double TimeWait = 0, TimePos = 0, RealPosition, RealTime = 0;
+        List<double> Time = new List<double>();
+        List<double> PointsX = new List<double>();
+        List<double> PointsY = new List<double>();
+        
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "txt files (*.txt)|*.txt|seq files (*.seq)|*.seq|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                PathRead = dlg.FileName;
+                using (FileStream fs = File.Open(PathRead, FileMode.Open))
+                {
+                    byte[] b = new byte[7777024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        InputFile = temp.GetString(b);
+                    }
+                }
+                string[] FileSplit = InputFile.Split(':', ';', ' ', '\n');
+                for (int i = 0; i < FileSplit.Length; i++)
+                {
+                    switch (FileSplit[i])
+                    {
+                        case "LOGic":
+                            switch (FileSplit[i + 1])
+                            {
+                                case "WAIT":
+                                    RealTime = TimeWait + TimePos;
+                                    TimeWait += Convert.ToDouble(FileSplit[i + 2]) / 1000;
+                                    while (RealTime < TimeWait)
+                                    {
+                                        PointsX.Add(RealTime);
+                                        PointsY.Add(LastPosition);
+                                        RealTime++;
+                                    }
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "WAITPos":
+                                    RealTime = TimeWait + TimePos;
+                                    TimeWait += Convert.ToDouble(FileSplit[i + 4]) / 1000;
+                                    while (RealTime < TimeWait)
+                                    {
+                                        PointsX.Add(RealTime);
+                                        PointsY.Add(LastPosition);
+                                        RealTime++;
+                                    }
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "WAITRate":
+                                    RealTime = TimeWait + TimePos;
+                                    TimeWait += Convert.ToDouble(FileSplit[i + 4]) / 1000;
+                                    while (RealTime < TimeWait)
+                                    {
+                                        PointsX.Add(RealTime);
+                                        PointsY.Add(LastPosition);
+                                        RealTime++;
+                                    }
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "WAITTemp":
+                                    RealTime = TimeWait + TimePos;
+                                    TimeWait += Convert.ToDouble(FileSplit[i + 4]) * 60;
+                                    while (RealTime < TimeWait)
+                                    {
+                                        PointsX.Add(RealTime);
+                                        PointsY.Add(LastPosition);
+                                        RealTime++;
+                                    }
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "WAITGForce":
+                                    RealTime = TimeWait + TimePos;
+                                    TimeWait += Convert.ToDouble(FileSplit[i + 4]) * 60;
+                                    while (RealTime < TimeWait)
+                                    {
+                                        PointsX.Add(RealTime);
+                                        PointsY.Add(LastPosition);
+                                        RealTime++;
+                                    }
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "Do":
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "LOOPUntil":
+                                    Time.Add(TimeWait);
+                                    break;
+                                case "GOTO":
+                                    Time.Add(TimeWait);
+                                    break;
+                            }
+                            break;
+                        case "DEMand":
+                            switch (FileSplit[i + 1])
+                            {
+                                case "POSition":
+                                    S = Math.Abs(Convert.ToDouble(FileSplit[i + 3]) - LastPosition);
+                                    V = Math.Abs(Convert.ToDouble(FileSplit[i + 4]));
+                                    A = Math.Abs(Convert.ToDouble(FileSplit[i + 5]));
+                                    RealPosition = LastPosition;
+                                    LastPosition = Convert.ToDouble(FileSplit[i + 3]);
+                                    if (S <= (V * V / (2 * A)))     //Если движение только равноускоренное
+                                    {
+                                        TimePos = Math.Sqrt(2 * S / A);
+                                        for (int j = 0; j < TimePos; j += 1)
+                                        {
+                                            PointsX.Add(TimeWait + j);
+                                            PointsY.Add(RealPosition + A*j*j/2);
+                                        }
+                                        
+                                        T += Math.Sqrt(2 * S / A);
+                                        Time.Add(T);
+                                    }
+                                    else
+                                    {
+                                        TimePos = (V / A) + ((S - (V * V / (2 * A))) / V);
+                                        for (int j = 0; j < TimePos; j += 1)
+                                        {
+                                            if ((RealPosition + A * j * j / 2) <= LastPosition)
+                                            {
+                                                PointsX.Add(TimeWait + j);
+                                                PointsY.Add(RealPosition + A * j * j / 2);
+                                            }
+                                            else
+                                            {
+                                                PointsX.Add(TimeWait + j);
+                                                PointsY.Add(LastPosition);
+                                            }
+                                        }
+                                        
+                                        T += (V / A) + ((S - (V * V / (2 * A))) / V);
+                                        Time.Add(T);
+                                    }
+                                    break;
+                                case "RATe":
+                                    Time.Add(T);
+                                    break;
+                                case "OSCillation":
+                                    Time.Add(T);
+                                    break;
+                                case "CURrent":
+                                    Time.Add(T);
+                                    break;
+                                case "GFOrce":
+                                    Time.Add(T);
+                                    break;
+                                case "TEMP":
+                                    Time.Add(T);
+                                    break;
+                            }
+                            break;
+                        case "INTerlock":
+                            Time.Add(T);
+                            break;
+                        case "USER":
+                            Time.Add(T);
+                            break;
+                        case "ADD":
+                            Time.Add(T);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                FinalTime = TimeWait;
+                ScrollBarXmax.Minimum = ScrollBarXmin.Minimum = 0;
+                ScrollBarXmax.Maximum = ScrollBarXmin.Maximum = (int)PointsX[Convert.ToInt32(PointsX.Count) - 1];
+            }
+        }
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string[] EndSplit = InputFile.Split('\n');
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.txt)|*.txt|seq files (*.seq)|*.seq|All files (*.*)|*.*";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                PathWrite = dialog.FileName;
+                using (StreamWriter SW = new StreamWriter(PathWrite, false, System.Text.Encoding.Default))
+                {
+                    for (int i = 0; i < Time.Count; i++)
+                    {
+                        SW.WriteLine(Convert.ToString(EndSplit[i]) + "Time: " + Convert.ToString(Time[i]));
+                    }
+                }
+            }
+        }
+
+        private void Grafik1_Click(object sender, EventArgs e)
+        {
+            this.chart1.Series[0].Points.Clear();
+            
+            if (ScrollBarXmax.Value > ScrollBarXmin.Value)
+            {
+                chart1.ChartAreas[0].AxisX.Minimum = ScrollBarXmin.Value;
+                chart1.ChartAreas[0].AxisX.Maximum = ScrollBarXmax.Value;
+                
+            }
+            for (int i = 0; i < PointsX.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY(PointsX[i], PointsY[i]);
+            }
+        }
+        private void chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var chart = (System.Windows.Forms.DataVisualization.Charting.Chart)sender;
+            var xAxis = chart.ChartAreas[0].AxisX;
+            var yAxis = chart.ChartAreas[0].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Scrolled down.
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+                else if (e.Delta > 0) // Scrolled up.
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+    }
+}
