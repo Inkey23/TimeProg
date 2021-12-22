5a99025420324af3ec0b5741468aa79ff99bb83c
andrewoculus <andrewoculus@gmail.com>
2021-12-22 17:56:58 +0300

    v1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,6 @@ namespace TimeProg
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.LoadButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
             this.Grafik1 = new System.Windows.Forms.Button();
@@ -44,14 +43,16 @@ namespace TimeProg
             this.CheckHorizontal = new System.Windows.Forms.CheckBox();
             this.CheckVertical = new System.Windows.Forms.CheckBox();
             this.CheckTemperature = new System.Windows.Forms.CheckBox();
+            this.CheckZoom = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.GrafikTable)).BeginInit();
             this.SuspendLayout();
             // 
             // LoadButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(12, 881);
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(1240, 517);
             this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(207, 45);
+            this.LoadButton.Size = new System.Drawing.Size(244, 45);
             this.LoadButton.TabIndex = 0;
             this.LoadButton.Text = "Загрузить скрипт";
             this.LoadButton.UseVisualStyleBackColor = true;
@@ -59,19 +60,21 @@ namespace TimeProg
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(1592, 881);
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(1240, 621);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(209, 45);
+            this.SaveButton.Size = new System.Drawing.Size(244, 45);
             this.SaveButton.TabIndex = 1;
-            this.SaveButton.Text = "Сохранить в файл";
+            this.SaveButton.Text = "Экспортировать данные";
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
             // Grafik1
             // 
-            this.Grafik1.Location = new System.Drawing.Point(827, 881);
+            this.Grafik1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grafik1.Location = new System.Drawing.Point(1240, 568);
             this.Grafik1.Name = "Grafik1";
-            this.Grafik1.Size = new System.Drawing.Size(275, 45);
+            this.Grafik1.Size = new System.Drawing.Size(244, 45);
             this.Grafik1.TabIndex = 2;
             this.Grafik1.Text = "Построить график";
             this.Grafik1.UseVisualStyleBackColor = true;
@@ -81,22 +84,35 @@ namespace TimeProg
             // 
             this.GrafikTable.BackColor = System.Drawing.Color.PaleTurquoise;
             this.GrafikTable.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.DiagonalLeft;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
             chartArea1.AxisX.Title = "Время (сек) ";
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisY2.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY2.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             chartArea1.Name = "ChartArea1";
             this.GrafikTable.ChartAreas.Add(chartArea1);
+            this.GrafikTable.Cursor = System.Windows.Forms.Cursors.Default;
+            this.GrafikTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            legend1.IsTextAutoFit = false;
             legend1.Name = "Legend1";
             this.GrafikTable.Legends.Add(legend1);
-            this.GrafikTable.Location = new System.Drawing.Point(16, 12);
+            this.GrafikTable.Location = new System.Drawing.Point(0, 0);
             this.GrafikTable.Name = "GrafikTable";
+            series1.BorderWidth = 2;
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.Legend = "Legend1";
             series1.Name = "Горизонтальная пл-ть";
+            series2.BorderWidth = 2;
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series2.Legend = "Legend1";
             series2.Name = "Вертикальная пл-ть";
             series2.YValuesPerPoint = 6;
+            series3.BorderWidth = 2;
             series3.ChartArea = "ChartArea1";
             series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series3.Legend = "Legend1";
@@ -105,22 +121,21 @@ namespace TimeProg
             this.GrafikTable.Series.Add(series1);
             this.GrafikTable.Series.Add(series2);
             this.GrafikTable.Series.Add(series3);
-            this.GrafikTable.Size = new System.Drawing.Size(1785, 863);
+            this.GrafikTable.Size = new System.Drawing.Size(1496, 678);
             this.GrafikTable.TabIndex = 3;
             this.GrafikTable.Text = "GrafikTable";
             title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
             title1.Name = "Title1";
-            title1.Text = "Циклограмма v0.4";
-            title2.Name = "Title2";
-            title2.Text = "Путь к файлу";
+            title1.Text = "Циклограмма v1.0";
+            title1.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
             this.GrafikTable.Titles.Add(title1);
-            this.GrafikTable.Titles.Add(title2);
             this.GrafikTable.Click += new System.EventHandler(this.chart1_Click);
             // 
             // CheckRate
             // 
+            this.CheckRate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CheckRate.AutoSize = true;
-            this.CheckRate.Location = new System.Drawing.Point(1569, 190);
+            this.CheckRate.Location = new System.Drawing.Point(1241, 428);
             this.CheckRate.Name = "CheckRate";
             this.CheckRate.Size = new System.Drawing.Size(117, 17);
             this.CheckRate.TabIndex = 5;
@@ -129,52 +144,70 @@ namespace TimeProg
             // 
             // CheckHorizontal
             // 
+            this.CheckHorizontal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CheckHorizontal.AutoSize = true;
             this.CheckHorizontal.Checked = true;
             this.CheckHorizontal.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckHorizontal.Location = new System.Drawing.Point(1569, 130);
+            this.CheckHorizontal.Location = new System.Drawing.Point(1241, 451);
             this.CheckHorizontal.Name = "CheckHorizontal";
-            this.CheckHorizontal.Size = new System.Drawing.Size(15, 14);
+            this.CheckHorizontal.Size = new System.Drawing.Size(204, 17);
             this.CheckHorizontal.TabIndex = 6;
+            this.CheckHorizontal.Text = "Показывать горизонтальную пл-ть";
             this.CheckHorizontal.UseVisualStyleBackColor = true;
             // 
             // CheckVertical
             // 
+            this.CheckVertical.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CheckVertical.AutoSize = true;
             this.CheckVertical.Checked = true;
             this.CheckVertical.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckVertical.Location = new System.Drawing.Point(1569, 145);
+            this.CheckVertical.Location = new System.Drawing.Point(1241, 474);
             this.CheckVertical.Name = "CheckVertical";
-            this.CheckVertical.Size = new System.Drawing.Size(15, 14);
+            this.CheckVertical.Size = new System.Drawing.Size(193, 17);
             this.CheckVertical.TabIndex = 7;
+            this.CheckVertical.Text = "Показывать вертикальную пл-ть";
             this.CheckVertical.UseVisualStyleBackColor = true;
             // 
             // CheckTemperature
             // 
+            this.CheckTemperature.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.CheckTemperature.AutoSize = true;
             this.CheckTemperature.Checked = true;
             this.CheckTemperature.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.CheckTemperature.Location = new System.Drawing.Point(1569, 160);
+            this.CheckTemperature.Location = new System.Drawing.Point(1241, 494);
             this.CheckTemperature.Name = "CheckTemperature";
-            this.CheckTemperature.Size = new System.Drawing.Size(15, 14);
+            this.CheckTemperature.Size = new System.Drawing.Size(156, 17);
             this.CheckTemperature.TabIndex = 8;
+            this.CheckTemperature.Text = "Показывать температуру";
             this.CheckTemperature.UseVisualStyleBackColor = true;
+            // 
+            // CheckZoom
+            // 
+            this.CheckZoom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CheckZoom.AutoSize = true;
+            this.CheckZoom.Location = new System.Drawing.Point(1241, 405);
+            this.CheckZoom.Name = "CheckZoom";
+            this.CheckZoom.Size = new System.Drawing.Size(249, 17);
+            this.CheckZoom.TabIndex = 9;
+            this.CheckZoom.Text = "Режим масшатабирования кнопками мыши";
+            this.CheckZoom.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1813, 938);
+            this.ClientSize = new System.Drawing.Size(1496, 678);
+            this.Controls.Add(this.CheckZoom);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.Grafik1);
+            this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.CheckTemperature);
             this.Controls.Add(this.CheckVertical);
             this.Controls.Add(this.CheckHorizontal);
             this.Controls.Add(this.CheckRate);
             this.Controls.Add(this.GrafikTable);
-            this.Controls.Add(this.Grafik1);
-            this.Controls.Add(this.SaveButton);
-            this.Controls.Add(this.LoadButton);
             this.Name = "Form1";
-            this.Text = "AcuitasCyclogram_v0.4";
+            this.Text = "AcuitasCyclogram_v1.0";
             ((System.ComponentModel.ISupportInitialize)(this.GrafikTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -191,6 +224,7 @@ namespace TimeProg
         private System.Windows.Forms.CheckBox CheckVertical;
         private System.Windows.Forms.CheckBox CheckTemperature;
         public System.Windows.Forms.DataVisualization.Charting.Chart GrafikTable;
+        private System.Windows.Forms.CheckBox CheckZoom;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,14 +17,17 @@ namespace TimeProg
         public Form1()
         {
             InitializeComponent();
+            WindowState = FormWindowState.Maximized;
             GrafikTable.MouseWheel += chart1_MouseWheel;
+            GrafikTable.MouseClick += chart1_MouseDown;
         }
         CommandClass MyCommand = new CommandClass();
 
         string PathRead = "0", InputFile = "0", PathWrite = "0";
         List<double> Time = new List<double>();
-        
-        
+        double LastTime;
+        int MaxPoint = 0;
+
         private void LoadButton_Click(object sender, EventArgs e)
         {
             var dlg = new OpenFileDialog();
@@ -183,18 +186,21 @@ namespace TimeProg
                 }
                 #endregion
                 #region AddPoints
-                double LastTime;
+                
                 if ((MyCommand.PointsX1[MyCommand.PointsX1.Count - 1] >= MyCommand.PointsX2[MyCommand.PointsX2.Count - 1]) && (MyCommand.PointsX1[MyCommand.PointsX1.Count - 1] >= MyCommand.PointsX3[MyCommand.PointsX3.Count - 1]))
                 {
                     LastTime = MyCommand.PointsX1[MyCommand.PointsX1.Count - 1];
+                    MaxPoint = 1;
                 }
                 else if ((MyCommand.PointsX2[MyCommand.PointsX2.Count - 1] >= MyCommand.PointsX1[MyCommand.PointsX1.Count - 1]) && (MyCommand.PointsX2[MyCommand.PointsX2.Count - 1] >= MyCommand.PointsX3[MyCommand.PointsX3.Count - 1]))
                 {
                     LastTime = MyCommand.PointsX2[MyCommand.PointsX2.Count - 1];
+                    MaxPoint = 2;
                 }
                 else
                 {
                     LastTime = MyCommand.PointsX3[MyCommand.PointsX3.Count - 1];
+                    MaxPoint = 3;
                 }
                 MyCommand.PointsX1.Add(LastTime);
                 MyCommand.PointsY1.Add(MyCommand.LastPosition[0]);
@@ -251,24 +257,108 @@ namespace TimeProg
                 }
                 */
                 #endregion 
+
+
             }
         }
         private void SaveButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Данная команда находится в стадии разработки");
             #region Save
-            string[] EndSplit = InputFile.Split('\n');
             var dialog = new OpenFileDialog();
+            int LastCount = 0;
+            double Time0 = 0, Point1 = 0, Point2 = 0, Point3 = 20, Rate1 = 0, Rate2 = 0, Rate3 = 0;
             dialog.Filter = "txt files (*.txt)|*.txt|seq files (*.seq)|*.seq|All files (*.*)|*.*";
             if (dialog.ShowDialog() == DialogResult.OK)
             {
                 PathWrite = dialog.FileName;
                 using (StreamWriter SW = new StreamWriter(PathWrite, false, System.Text.Encoding.Default))
                 {
-                    for (int i = 0; i < Time.Count; i++)
+                    SW.WriteLine("Время(сек):  " + "Позиция горизонтальной пл-ти(град):  " + "Позиция вертикальной пл-ти(град):  " + "Позиция температуры(°С):  "
+                        + "Скорость горизонтальной пл-ти(град/сек):  " + "Скорость вертикальной пл-ти(град/сек):  " + "Скорость изменения температуры(°С/мин):  ");
+                    if (MaxPoint == 1)
                     {
-                        SW.WriteLine(Convert.ToString(EndSplit[i]) + "Time: " + Convert.ToString(Time[i]));
+                        LastCount = MyCommand.PointsX1.Count;
                     }
+                    else if (MaxPoint == 2)
+                    {
+                        LastCount = MyCommand.PointsX2.Count;
+                    }
+                    else if (MaxPoint == 3)
+                    {
+                        LastCount = MyCommand.PointsX3.Count;
+                    }
+                    for (int i = 0; i < LastCount; i++)
+                    {
+                        if (i < MyCommand.PointsX1.Count)
+                        {
+                            Time0 = MyCommand.PointsX1[i];
+                        }
+                        else if (i < MyCommand.PointsX2.Count)
+                        {
+                            Time0 = MyCommand.PointsX2[i];
+                        }
+                        else
+                        {
+                            Time0 = MyCommand.PointsX3[i];
+                        }
+
+                        if (i < MyCommand.PointsY1.Count)
+                        {
+                            Point1 = MyCommand.PointsY1[i];
+                        }
+                        else
+                        {
+                            Point1 = 0;
+                        }
+
+                        if (i < MyCommand.PointsY2.Count)
+                        {
+                            Point2 = MyCommand.PointsY2[i];
+                        }
+                        else
+                        {
+                            Point2 = 0;
+                        }
+
+                        if (i < MyCommand.PointsY3.Count)
+                        {
+                            Point3 = MyCommand.PointsY3[i];
+                        }
+                        else
+                        {
+                            Point3 = 20;
+                        }
+
+                        if (i < MyCommand.RateY1.Count)
+                        {
+                            Rate1 = MyCommand.RateY1[i];
+                        }
+                        else
+                        {
+                            Rate1 = 0;
+                        }
+
+                        if (i < MyCommand.RateY2.Count)
+                        {
+                            Rate2 = MyCommand.RateY2[i];
+                        }
+                        else
+                        {
+                            Rate2 = 0;
+                        }
+
+                        if (i < MyCommand.RateY3.Count)
+                        {
+                            Rate3 = MyCommand.RateY3[i];
+                        }
+                        else
+                        {
+                            Rate3 = 0;
+                        }
+                        SW.WriteLine(Time0 + ";  " + Point1 + ";  " + Point2 + ";  " + Point3
+                            + ";  " + Rate1 + ";  " + Rate2 + ";  " + Rate3 + ";");
+                    }
+
                 }
             }
             #endregion
@@ -277,12 +367,13 @@ namespace TimeProg
         {
             if(MyCommand.ErrorFlag == true)
             {
-                MessageBox.Show("Были обнаружены ошибки скрипта. Пожалуйста, проверьте консоль");
+                MessageBox.Show("В скрипте были обнаружены нестандартные решения, что может привести к ошибкам в построении графика. " +
+                    "Причиной являются непрописанные в коде (редкие) команды, или попытка использовать position при ненулевом значении rate. ");
             }
-            GrafikTable.Titles.Remove(GrafikTable.Titles[1]);
-            GrafikTable.Titles.Add(PathRead);
+            GrafikTable.Titles[0].Text = PathRead;
             GrafikTable.ChartAreas[0].AxisX.Minimum = 0;
             GrafikTable.ChartAreas[0].AxisX.Title = "Время (сек)";
+
             #region Drawing
             this.GrafikTable.Series[0].Points.Clear();
             this.GrafikTable.Series[1].Points.Clear();
@@ -347,14 +438,19 @@ namespace TimeProg
             #region Zoom
             GrafikTable.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
             GrafikTable.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
-            
+            //GrafikTable.ChartAreas[0].AxisY2.ScaleView.Zoomable = true;
+
+
             var chart = (System.Windows.Forms.DataVisualization.Charting.Chart)sender;
             var xAxis = chart.ChartAreas[0].AxisX;
             var yAxis = chart.ChartAreas[0].AxisY;
+            //var y2Axis = chart.ChartAreas[0].AxisY2;
             var xMin = xAxis.ScaleView.ViewMinimum;
             var xMax = xAxis.ScaleView.ViewMaximum;
             var yMin = yAxis.ScaleView.ViewMinimum;
             var yMax = yAxis.ScaleView.ViewMaximum;
+            //var y2Min = y2Axis.ScaleView.ViewMinimum;
+            //var y2Max = y2Axis.ScaleView.ViewMaximum;
             try
             {
                 if (e.Delta < 0) // Scrolled down.
@@ -384,16 +480,52 @@ namespace TimeProg
             catch { }
             #endregion
         }
-        private void button1_MouseDown(object sender, MouseEventArgs e)
+        private void chart1_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.Button == MouseButtons.Left)
+            #region ZoomClick
+            if (CheckZoom.Checked)
             {
-                MessageBox.Show("Вы нажали Левую кнопку, координаты курсора - " + e.Location);
+                GrafikTable.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+                GrafikTable.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+                var chart = (System.Windows.Forms.DataVisualization.Charting.Chart)sender;
+                var xAxis = chart.ChartAreas[0].AxisX;
+                var yAxis = chart.ChartAreas[0].AxisY;
+                var xMin = xAxis.ScaleView.ViewMinimum;
+                var xMax = xAxis.ScaleView.ViewMaximum;
+                var yMin = yAxis.ScaleView.ViewMinimum;
+                var yMax = yAxis.ScaleView.ViewMaximum;
+                try
+                {
+                    if (e.Button == MouseButtons.Right) // Уменьшить
+                    {
+                        //xAxis.ScaleView.ZoomReset();
+                        //yAxis.ScaleView.ZoomReset();
+                        var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) * 1.5;
+                        var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) * 1.5;
+                        var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) * 1.5;
+                        var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) * 1.5;
+
+                        xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                        yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                    }
+                    else if (e.Button == MouseButtons.Left) // Увеличить
+                    {
+
+                        var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 3;
+                        var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 3;
+                        var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 3;
+                        var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 3;
+
+                        xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                        yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                    }
+                }
+                catch { }
             }
+            #endregion
         }
         private void chart1_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
